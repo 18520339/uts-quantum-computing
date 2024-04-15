0e1271d3df8ed68916dd236e53003b4b6259dfbb
Quan Dang <18520339@gm.uit.edu.vn>
2024-04-16 03:11:01 +1000

    Refactor GUI layout

--- gui.py
+++ gui.py
@@ -15,7 +15,7 @@ class QuantumTicTacToeGUI:
         self.quantum_move_mode = 'CLASSICAL'
         self.quantum_moves_selected = [] # Selected cells for operation on multi-qubit gates 
 
-        self.log = widgets.Output()
+        self.log = widgets.Output(layout={'margin': '10px 0 0 0'})
         self.circuit_output = widgets.Output()
         self.histogram_output = widgets.Output()
         self.create_widgets()
@@ -29,56 +29,53 @@ class QuantumTicTacToeGUI:
             for j in range(self.board.size):
                 button = widgets.Button(
                     description = ' ', 
-                    layout = {'width': '120px', 'height': '120px', 'border': '1px solid black'}
+                    layout = {'width': '100px', 'height': '100px', 'border': '1px solid black'},
                     style = {'button_color': 'lightgray', 'font_weight': 'bold'}
                 )
                 button.on_click(self.create_on_cell_clicked(i, j))
                 self.buttons[i].append(button)
 
-        self.game_info = widgets.HTML(f"<h3>Turn: {self.current_player} - Mode: {self.quantum_move_mode}</h3>")
+        self.create_action_buttons()
+        self.game_info = widgets.HTML(f'<b>Current Player: {self.current_player} / Quantum Mode: {self.quantum_move_mode}</b>')
+        
         self.board_histogram_widget = widgets.HBox([
             widgets.VBox([
                 widgets.VBox([widgets.HBox(row) for row in self.buttons]), 
-                self.game_info, self.log
+                self.game_info
             ]), 
             self.histogram_output
-        ], layout = {'display': 'flex', 'justify_content': 'space-between'})
-
-        self.create_action_buttons()
-        display(widgets.VBox([self.board_histogram_widget, self.action_buttons, self.circuit_output]))
+        ], layout = {'display': 'flex', 'justify_content': 'space-between', 'align_items': 'flex-end'})
+        display(widgets.VBox([self.board_histogram_widget, self.action_buttons, self.log, self.circuit_output]))
 
 
     def create_action_buttons(self):
         # Create buttons for each action
-        self.classical_btn = widgets.Button(description='Classical Move', button_style='primary')
-        self.swap_btn = widgets.Button(description='SWAP Move', button_style='info')
-        self.entangled_btn = widgets.Button(description='Entanglement', button_style='success')
+        self.reset_btn = widgets.Button(description='Reset')
         self.measure_btn = widgets.Button(description='Measure', button_style='warning')
-        self.reset_btn = widgets.Button(description='Reset', button_style='danger')
+        self.classical_btn = widgets.Button(description='Classical Move', button_style='danger')
+        self.swap_btn = widgets.Button(description='SWAP Move', button_style='success')
+        
+        self.superposition_btn = widgets.Button(description='Superposition', button_style='info')
+        self.entangled_btn = widgets.Button(description='Entanglement', button_style='primary')
+        self.entangled_options = widgets.Dropdown(
+            options = [('PAIRWAISE: Hadamard-CNOT', 1), ('TRIPLE: Hadamard-CNOT-TOFFOLI', 2)], 
+            value = 1 # Default is Pairwise Entanglement
+        )
         
         # Assign the on-click events to the buttons
+        self.reset_btn.on_click(self.on_reset_btn_clicked)
+        self.measure_btn.on_click(self.on_measure_btn_clicked)
         self.classical_btn.on_click(self.create_on_quantum_clicked('CLASSICAL'))
         self.swap_btn.on_click(self.create_on_quantum_clicked('SWAP', 'Select 2 cells to swap their states.'))
         self.entangled_btn.on_click(self.create_on_quantum_clicked('ENTANGLED', 'Select 2 cells to entangle.'))
-        self.measure_btn.on_click(self.on_measure_btn_clicked)
-        self.reset_btn.on_click(self.on_reset_btn_clicked)
         
         # Arrange the buttons in a horizontal layout
-        self.action_buttons = widgets.HBox(
-            [self.classical_btn, self.swap_btn, self.entangled_btn, self.measure_btn, self.reset_btn],
-            layout = {'margin': '10px 0px 10px 0px'}
-        )
+        self.action_buttons = widgets.VBox([
+            widgets.HBox([self.reset_btn, self.measure_btn, self.classical_btn, self.swap_btn]),
+            widgets.HBox([self.entangled_options, self.entangled_btn, self.superposition_btn]),
+        ])
     
     
-    def on_measure_btn_clicked(self, btn=None):
-        with self.log:
-            clear_output(wait=True)
-            counts = self.board.collapse_board()
-            self.display_histogram(counts)
-            self.update_board()
-            print('Board measured and quantum states collapsed.')
-            
-            
     def on_reset_btn_clicked(self, btn=None):
         with self.log:
             clear_output(wait=True)
@@ -88,15 +85,25 @@ class QuantumTicTacToeGUI:
             self.quantum_move_mode = 'CLASSICAL'
             self.update_board()
             print('Game reset. New game started.')
+            
+            
+    def on_measure_btn_clicked(self, btn=None):
+        with self.log:
+            clear_output(wait=True)
+            counts = self.board.collapse_board()
+            self.display_histogram(counts)
+            self.update_board()
+            print('Board measured and quantum states collapsed.')
     
     
     def create_on_quantum_clicked(self, mode, message=''):
         def on_quantum_clicked(btn):
             with self.log:
+                clear_output(wait=True)
                 self.quantum_move_mode = mode
                 self.quantum_moves_selected = []
-                self.game_info.value = f"<h3>Turn: {self.current_player} - Mode: {self.quantum_move_mode}</h3>"
-                print(f'{mode} mode ACTIVATED' + f': {message}' if message else '')
+                self.game_info.value = f'<b>Current Player: {self.current_player} / Quantum Mode: {self.quantum_move_mode}</b>'
+                print(f'{mode} mode ACTIVATED' + (f': {message}' if message else ''))
         return on_quantum_clicked
     
 
@@ -148,13 +155,13 @@ class QuantumTicTacToeGUI:
             for j in range(self.board.size):
                 cell = self.board.cells[i][j]
                 button = self.buttons[i][j]
-                color_map = {'X': 'dodgerblue', 'O': 'orangered', '?': 'green', ' ': 'lightgray'}
+                color_map = {'X': 'orangered', 'O': 'green', '?': 'dodgerblue', ' ': 'lightgray'}
                 
                 button.description = cell if cell != ' ' else ' '
                 button.style.button_color = color_map[cell[-1]]
                 button.disabled = self.game_over
-        
-        self.game_info.value = f"<h3>Turn: {self.current_player} - Mode: {self.quantum_move_mode}</h3>"
+                
+        self.game_info.value = f'<b>Current Player: {self.current_player} / Quantum Mode: {self.quantum_move_mode}</b>'
         for btn in self.action_buttons.children[:-1]:
             btn.disabled = self.game_over
         
@@ -185,4 +192,4 @@ class QuantumTicTacToeGUI:
     def display_histogram(self, counts):
         with self.histogram_output:
             clear_output(wait=True)
-            display(plot_histogram(counts, figsize=(8, 5)))
+            display(plot_histogram(counts, figsize=(9, 5)))
