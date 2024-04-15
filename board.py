f8aa0d45717fd687f8ab314ed4e057a45ac6148b
Quan Dang <18520339@gm.uit.edu.vn>
2024-04-15 15:27:04 +1000

    Add classical and entangled move methods to Board

--- board.py
+++ board.py
@@ -24,3 +24,22 @@ class Board:
                              [tuple(range(i * size, (i + 1) * size)) for i in range(size)] + \
                              [tuple(range(0, size**2, size + 1)), tuple(range(size - 1, size**2 - 1, size - 1))]
                               
+
+    def make_classical_move(self, row, col, symbol):
+        if self.cells[row][col] == ' ': # Check if the cell is occupied
+            self.cells[row][col] = symbol
+            self.circuit.x(self.qubits[row * self.size + col]) # Pauli-X to mark the move
+            return True
+        return False
+
+
+    def make_entangled_move(self, row1, col1, row2, col2, symbol):
+        # Entangle 2 cells by applying a Hadamard gate and a CNOT gate
+        if self.cells[row1][col1] == ' ' and self.cells[row2][col2] == ' ' and (row1, col1) != (row2, col2):
+            indices = [row1 * self.size + col1, row2 * self.size + col2]
+            self.circuit.h(self.qubits[indices[0]])
+            self.circuit.cx(self.qubits[indices[0]], self.qubits[indices[1]])
+            self.cells[row1][col1] = self.cells[row2][col2] = symbol + '?'
+            self.entanglement_count += 1
+            return True
+        return False
